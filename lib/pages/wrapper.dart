3bd4ab0c703c4b77302664a01bf0c3e636450ef8
Mehedi <mehedihasanmax95@gmail.com>
2021-07-19 22:13:50 +0600

    homePage updated

--- lib/pages/wrapper.dart
+++ lib/pages/wrapper.dart
@@ -1,5 +1,5 @@
 import 'package:covid_19_tracker/helpers/size_config/size_config.dart';
-import 'package:covid_19_tracker/pages/HomePage.dart';
+import 'package:covid_19_tracker/pages/home/HomePage.dart';
 import 'package:covid_19_tracker/pages/user_credentials/sign_in.dart/log_in_page.dart';
 import 'package:covid_19_tracker/pages/user_credentials/sign_up.dart/sign_up_page.dart';
 import 'package:flutter/material.dart';
